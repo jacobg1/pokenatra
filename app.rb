46a4c62c6d57890bfe573b8dea72775d719a121d
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-25 14:42:08 -0400

    add active record

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'active_record'
+
+require_relative 'db/connection'
+
+require_relative 'models/pokemon'
+
+
+get '/' do
+  puts 'Hello'
+  erb :index
+end
