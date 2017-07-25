69b9b169ce54ff115e01d28c88e9bf957f2c5baa
Jacob Greenwald <jeg4t@virginia.edu>
2017-07-25 15:08:34 -0400

    add basic view / show

--- app.rb
+++ app.rb
@@ -6,8 +6,22 @@ require_relative 'db/connection'
 
 require_relative 'models/pokemon'
 
-
-get '/' do
-  puts 'Hello'
+#pokemon index
+get '/pokemon' do
+  @pokemon = Pokemon.all
   erb :index
+end
+
+#add new pokemon page (get)
+get '/pokemon/new' do
+  erb :new
+end
+
+#add new pokemon (post)
+
+
+#pokemon show page
+get '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :show
 end
